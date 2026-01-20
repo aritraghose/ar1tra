1074de4a51ad70fd3f0e7f9c58327daaf35a1579
Aritra Ghose <ar1tragh05e9@gmail.com>
2026-01-20 16:33:16 +0600

    ugggggggh

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "ar1tra.com",
+  site: "https://ar1tra.com",
   vite: {
     plugins: [tailwindcss()]
   },
