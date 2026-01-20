5d8b9f5aba927fbadfc9199b108409f5a2b72836
Aritra Ghose <ar1tragh05e9@gmail.com>
2026-01-20 16:29:59 +0600

    cloudflare stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  site: "ar1tra.com",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
